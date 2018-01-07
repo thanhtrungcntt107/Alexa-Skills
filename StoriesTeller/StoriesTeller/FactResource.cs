e4d62a7fc190edb8ed98cbb8e71d289064cd544f
Trung Ho <thanhtrungcntt107@gmail.com>
2018-01-07 20:44:38 +0700

    Add project Stories Teller

---
+++ StoriesTeller/StoriesTeller/FactResource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoriesTeller
+{
+    public class FactResource
+    {
+        public FactResource(string language)
+        {
+            this.Language = language;
+            this.Facts = new List<string>();
+        }
+
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public List<string> Facts { get; set; }
+        public string GetFactMessage { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
+    }
+}
