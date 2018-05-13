bcba051847c2e9653a57f05e6ca69ae0c84cf519
Trung Ho <thanhtrungcntt107@gmail.com>
2018-05-13 23:28:05 +0700

    Revert "Test1"
    This reverts commit 653a4359899cf687e4c57bc0d9171dc7a79ad3fc.

--- StoriesTeller/CommonHelper/Test.cs
+++ StoriesTeller/CommonHelper/Test.cs
@@ -4,10 +4,6 @@ namespace CommonHelper
 {
     public static class Commonhek
     {
-        public void Test()
-        {
-            // code here
-        }
         private  string test_t;
     }
 }
