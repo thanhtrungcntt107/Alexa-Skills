653a4359899cf687e4c57bc0d9171dc7a79ad3fc
Trung Ho <thanhtrungcntt107@gmail.com>
2018-05-13 23:19:17 +0700

    Test1

--- StoriesTeller/CommonHelper/Test.cs
+++ StoriesTeller/CommonHelper/Test.cs
@@ -4,6 +4,10 @@ namespace CommonHelper
 {
     public static class Commonhek
     {
+        public void Test()
+        {
+            // code here
+        }
         private  string test_t;
     }
 }
